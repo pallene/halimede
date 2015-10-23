71e80dd27aa6fd7c8fdad7571f66ff897d23c95f
Raphael Cohn <raphael.cohn@stormmq.com>
2015-10-23 11:47:26 +0100

    bugfix

--- io/ShellLanguage.lua
+++ io/ShellLanguage.lua
@@ -4,8 +4,9 @@ Copyright © 2015 The developers of halimede. See the COPYRIGHT file in the top-
 ]]--
 
 
-local type = require('halimede').type
-local assert = require('halimede').assert
+local halimede = require('halimede')
+local type = halimede.type
+local assert = halimede.assert
 local tabelize = require('halimede.tabelize').tabelize
 local operatingSystemDetails = require('halimede').operatingSystemDetails
 local exception = require('halimede.exception')
