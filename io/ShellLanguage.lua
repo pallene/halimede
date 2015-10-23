79a21d2caeea118ca67487cc6b93771102e9061a
Raphael Cohn <raphael.cohn@stormmq.com>
2015-10-23 10:37:30 +0100

    Fix to quote escaping for POSIX

--- io/ShellLanguage.lua
+++ io/ShellLanguage.lua
@@ -6,10 +6,13 @@ Copyright © 2015 The developers of halimede. See the COPYRIGHT file in the top-
 
 local assert = require('halimede.assert')
 local tabelize = require('halimede.tabelize').tabelize
+local operatingSystemDetails = require('halimede').operatingSystemDetails
+local exception = require('halimede.exception').exception
 
 
 assert.globalTableHasChieldFieldOfTypeFunction('string', 'gsub')
 module.POSIX = {
+	pathSeparator = ':',
 	toShellCommand = function(...)
 		local arguments = {...}
 	
@@ -29,7 +32,32 @@ module.POSIX = {
 }
 
 module.Windows = {
+	pathSeparator = ';',
 	toShellCommand = function(...)
+
+		local arguments = {...}
+	
+		local commandBuffer = tabelize()
 		
+		-- http://lua-users.org/lists/lua-l/2013-11/msg00367.html
+		commandBuffer:insert('type NUL && ')
+	
+		-- Windows: add  "type NUL && "
+	
+		for _, argument in ipairs(arguments) do
+			assert.parameterTypeIsString(argument)
+			commandBuffer:insert("'" .. argument:gsub("'", "'\\''") .. "'")
+		end
+	
+		local command = commandBuffer:concat(' ')
+		return command
 	end
 }
+
+if operatingSystemDetails.isPOSIX then
+	module.Default = module.POSIX
+elseif operatingSystemDetails.isWindows then
+	module.Default = module.Windows
+else
+	exception.throw('Could not determine ShellLanguage')
+end
