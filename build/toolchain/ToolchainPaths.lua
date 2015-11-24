9f4ad51e8397eda1450ee5bea68a172379864d31
Raph Cohn <raphael.cohn@stormmq.com>
2015-11-24 16:38:00 +0000

    formatting

--- build/toolchain/ToolchainPaths.lua
+++ build/toolchain/ToolchainPaths.lua
@@ -4,7 +4,6 @@ Copyright © 2015 The developers of halimede. See the COPYRIGHT file in the top-
 ]]--
 
 
-
 --[[
 
 Some non-configure based installations (eg Lua and Shellfire) can only use relative paths for things like libdir. They can probably use '..' but will need patching
--- init/trace.lua
+++ init/trace.lua
@@ -4,7 +4,6 @@ Copyright © 2015 The developers of halimede. See the COPYRIGHT file in the top-
 ]]--
 
 
-
 local function traceIfRequired()
 	
 	local environmentVariable = 'LUA_HALIMEDE_TRACE'
