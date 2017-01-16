4cf7e5cf434b742a53f292c74d46c89d337d13ca
Raphael Cohn <raphael.cohn@stormmq.com>
2017-01-16 15:37:43 +0000

    Adding copyright

--- .dependencies/source/__main.c
+++ .dependencies/source/__main.c
@@ -1,8 +1,12 @@
-#include <stdio.h>
+// This file is part of halimede. It is subject to the licence terms in the COPYRIGHT file found in the top-level directory of this distribution and at https://raw.githubusercontent.com/pallene/halimede/master/COPYRIGHT. No part of halimede, including this file, may be copied, modified, propagated, or distributed except according to the terms contained in the COPYRIGHT file.
+// Copyright © 2015 The developers of halimede. See the COPYRIGHT file in the top-level directory of this distribution and at https://raw.githubusercontent.com/pallene/halimede/master/COPYRIGHT.
 
+
+#include <stdio.h>
 #include "lua.h"
 #include "lauxlib.h"
 #include "lualib.h"
+
 
 int main(const int argc, char **argv)
 {
