cf70ddfa3b8010fba7001738b61a8642ad4a9fd2
Raphael Cohn <raphael.cohn@stormmq.com>
2017-01-16 15:35:20 +0000

    New runtime logic

--- .dependencies/source/__main.c
+++ .dependencies/source/__main.c
@@ -6,12 +6,24 @@
 
 int main(const int argc, char **argv)
 {
-	int status;
 	lua_State *L = luaL_newstate();
 	luaL_openlibs(L);
+	
+	// Create _G.arg
+	int argument;
+	lua_createtable(L, argc, 1);
+	for (argument = 0; argument < argc; argument++)
+	{
+		lua_pushstring(L, argv[argument]);
+		lua_rawseti(L, -2, argument);
+	}
+	lua_setglobal(L, "arg");
+	luaL_checkstack(L, argc - 1, "Stack can not grow");
+	
+	// Run
 	lua_getglobal(L, "require");
 	lua_pushliteral(L, "__main");
-	status = lua_pcall(L, 1, 0, 0);
+	int status = lua_pcall(L, 1, 0, 0);
 	if (status)
 	{
 		fprintf(stderr, "Error:%s\n", lua_tostring(L, -1));
